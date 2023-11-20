e19e43bdfcbddfecd2879e750dab15222e063be5
bradfish29 <duongkhai2904@gmail.com>
2023-11-20 15:05:39 +0700

    First Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AzureDevops_FirstProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
